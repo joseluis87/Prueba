d8157882c1aeb22c216d348c27ccab8097a3640b
joseluis87 <jlaa1@hotmail.com>
2022-02-22 19:21:44 +0000

    subiendo multiplicacion y division

--- src/main/java/prueba.java
+++ src/main/java/prueba.java
@@ -9,4 +9,24 @@
  */
 public class prueba {
     
+    
+        public static int Multiplicacion(int a, int b) {
+        return a*b;
+    }
+    public static int Division(int a, int b) {
+        return a/b;
+    }
+    
+        
+    public static void main(String args[])
+    {
+
+        
+        System.out.println("Multiplicación de 1 * 3 = "+ Multiplicacion(1,3));
+        System.out.println("Division de 3 / 3 = "+ Division(3,3));
+
+    }
+    
+    
+    
 }
