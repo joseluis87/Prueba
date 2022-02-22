42631423d17a75bf83ae9b19d50f418801ac0dc1
joseluis87 <jlaa1@hotmail.com>
2022-02-22 19:16:59 +0000

    subiendo sumas y restas

--- src/main/java/prueba.java
+++ src/main/java/prueba.java
@@ -9,4 +9,21 @@
  */
 public class prueba {
     
+    
+        public static int Suma(int a, int b) {
+        return a+b;
+    }
+    public static int Resta(int a, int b) {
+        return a-b;
+    }
+    
+        public static void main(String args[])
+    {
+        System.out.println("Suma de 1 + 3 = "+ Suma(1,3));
+        System.out.println("Resta de 3 - 1 = "+ Resta(3,1));
+        
+        
+    }
+    
+    
 }
